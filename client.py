4eb00c90631e0940edba7379bd08d50388590ba5
Steven <github@ssloss.com>
2023-08-31 16:40:08 -0700

    Fixed bug in client.py where I forgot to set the server_hostname parameter. Runs end-to-end now.

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ def connect_to_server():
     # Create a socket and connect to the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Wrap the socket in an SSL context
-        with context.wrap_socket(s, server_side=False) as ssl_sock:
+        with context.wrap_socket(s, server_side=False, server_hostname='localhost') as ssl_sock:
             ssl_sock.connect(('localhost', 12345))
             
             # Check if server's certificate OU is as expected
@@ -24,6 +24,7 @@ def connect_to_server():
             print(f"Connected to server: {server_cert['subject']}")
             # Add your client logic here
             # ...
+
 
 if __name__ == "__main__":
     connect_to_server()
