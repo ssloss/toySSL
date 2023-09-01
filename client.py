318d5b6d3fe2693cf1d77fd6f76ac84c63d5a7c0
Steven <github@ssloss.com>
2023-08-31 17:09:17 -0700

    We now generate two certs and only trust the one with the correct OU

--- client.py
+++ client.py
@@ -1,33 +1,29 @@
 import ssl
 import socket
+import sys
 
-# Create a default SSL context
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+def load_context(cert_file, key_file):
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.load_cert_chain(certfile=cert_file, keyfile=key_file)
+    context.load_verify_locations(cafile="ca.crt")
+    return context
 
-# Load client's certificate and key
-context.load_cert_chain(certfile="client.crt", keyfile="client.key")
-
-# Load the CA certificate to verify the server's certificate
-context.load_verify_locations(cafile="ca.crt")
-
-def connect_to_server():
-    # Create a socket and connect to the server
+def connect_to_server(cert_choice):
+    cert_file, key_file = ("client1.crt", "client1.key") if cert_choice == "1" else ("client2.crt", "client2.key")
+    context = load_context(cert_file, key_file)
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Wrap the socket in an SSL context
         with context.wrap_socket(s, server_side=False, server_hostname='localhost') as ssl_sock:
             ssl_sock.connect(('localhost', 12345))
-            
-            # Check if server's certificate OU is as expected
             server_cert = ssl_sock.getpeercert()
-            # Perform additional checks on server_cert if needed
-            
             print(f"Connected to server: {server_cert['subject']}")
-
-            # Toy echo client just for demonstration purposes
+            
             while True:
                 msg = input("Enter a message to send to server (type 'exit' to quit): ")
                 ssl_sock.sendall(msg.encode('utf-8'))
                 if msg.lower() == 'exit':
                     break
+
 if __name__ == "__main__":
-    connect_to_server()
+    cert_choice = input("Which client certificate would you like to use? (1 for AllowedOU, 2 for WrongOU): ")
+    connect_to_server(cert_choice)
--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Create Root CA Certificate
+##############################
+# Create Root CA Certificate #
+##############################
 
 # Generate private key for root CA
 openssl genpkey -algorithm RSA -out ca.key
@@ -19,15 +21,25 @@ openssl req -new -key server.key -out server.csr -subj "/CN=localhost/O=My Organ
 # Sign the server CSR with the root CA key
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 
-# Create Client Certificate with OU='AllowedOU'
+##############################
+# Create Client Certificates #
+##############################
+# Generate private key for the first client
+openssl genpkey -algorithm RSA -out client1.key
 
-# Generate private key for the client
-openssl genpkey -algorithm RSA -out client.key
+# Create Certificate Signing Request (CSR) for the first client
+openssl req -new -key client1.key -out client1.csr -subj "/CN=Client1/O=My Organization/OU=AllowedOU/C=US"
 
-# Create Certificate Signing Request (CSR) for the client
-openssl req -new -key client.key -out client.csr -subj "/CN=Client/O=My Organization/OU=AllowedOU/C=US"
+# Sign the first client CSR with the root CA key
+openssl x509 -req -in client1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client1.crt
 
-# Sign the client CSR with the root CA key
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt
+# Generate private key for the second client
+openssl genpkey -algorithm RSA -out client2.key
+
+# Create Certificate Signing Request (CSR) for the second client
+openssl req -new -key client2.key -out client2.csr -subj "/CN=Client2/O=My Organization/OU=WrongOU/C=US"
+
+# Sign the second client CSR with the root CA key
+openssl x509 -req -in client2.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client2.crt
 
 echo "Certificates generated successfully."
